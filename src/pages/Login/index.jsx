2bb49ab80bf49852f231ff707216e341c771459a
VicWan <vwan56349@gmail.com>
2025-05-05 16:43:54 +0800

    login page with card component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,2 +1,16 @@
-const Login = () => <div>this is Login</div>
+import { Card, Form } from 'antd'
+import logo from '../../assets/logo192.png'
+import './index.scss'
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        <p> card content</p>
+      </Card>
+
+      <div>this is Login</div>
+    </div>
+  )
+}
 export default Login
