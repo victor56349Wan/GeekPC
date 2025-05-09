ddea484ea1afc1ad76774704aec260c942318597
VicWan <vwan56349@gmail.com>
2025-05-09 18:05:25 +0800

    try 'git commit -a -v'

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -51,6 +51,7 @@ const Article = () => {
         })
       }
     }
+    // load article list in useEffect once dependency changes
     loadArticleList()
   }, [params])
   const renderStatus = (status) => {
